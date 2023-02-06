205cf49c5cee3930c7d2303a8891c587232b3962
Marco Polo <seguros.marcopolo@gmail.com>
2023-02-06 19:09:58 -0300

    requi 1 lint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const routerTalkers = require('./RouterTalkers')
+const routerTalkers = require('./RouterTalkers');
 
 const app = express();
 app.use(express.json());
